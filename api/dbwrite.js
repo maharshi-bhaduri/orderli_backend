f4bcf840131c9b4752def15797ca286bac436403
Maharshi Bhaduri <bhaduri.maharshi@gmail.com>
2023-06-24 03:00:15 +0530

    test

--- api/dbwrite.js
+++ api/dbwrite.js
@@ -72,3 +72,17 @@ export async function getMenuItems(req, res) {
         res.status(500).json({ error: 'An error occurred' });
     }
 }
+
+module.exports = (req, res) => {
+    try {
+        const menuItems = await prisma.menu.findMany();
+
+        res.status(200).json(menuItems);
+    } catch (error) {
+        console.error('Error fetching menu items:', error);
+        res.status(500).json({ error: 'An error occurred' });
+    }
+    res.json({
+        hello: "worlds",
+    });
+};
