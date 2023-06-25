24362469c83cfec79ea6ddcf5f948db79263467c
Maharshi Bhaduri <bhaduri.maharshi@gmail.com>
2023-06-26 00:24:49 +0530

    removed comments

--- utils/utils.js
+++ utils/utils.js
@@ -11,8 +11,6 @@ function resUtil(res, statuscode, message, data) {
 
 const allowCors = fn => async (req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', true)
-    // res.setHeader('Access-Control-Allow-Origin', '*')
-    // // another common pattern
     res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
     res.setHeader(
