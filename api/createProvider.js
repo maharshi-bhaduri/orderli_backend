1186717b1219e8b2ef822d166a369510701e69c9
xav1994das <xav0anime@gmail.com>
2023-06-26 15:05:49 +0530

    added 'await' in create prover js which was removed by maharshi

--- api/createProvider.js
+++ api/createProvider.js
@@ -1,53 +1,60 @@
 // Serverless function to create a new provider
 
-import { PrismaClient } from '@prisma/client';
-import { allowCors, resUtil } from '../utils/utils';
-import qrcode from 'qrcode';
+import { PrismaClient } from "@prisma/client";
+import { allowCors, resUtil } from "../utils/utils";
+import qrcode from "qrcode";
 
 const prisma = new PrismaClient();
 
 const handler = async (req, res) => {
-    try {
-        const { providerName, providerType, providerHandle, about, address, city, state, country, postalCode, owner, website } = req.body;
-        prisma.provider_details.create({
-            data: {
-                provider_name: providerName,
-                provider_type: providerType,
-                provider_handle: providerHandle,
-                about,
-                address,
-                city,
-                state,
-                country,
-                postal_code: postalCode,
-                owner,
-                website,
-            },
-        });
+  try {
+    const {
+      providerName,
+      providerType,
+      providerHandle,
+      about,
+      address,
+      city,
+      state,
+      country,
+      postalCode,
+      owner,
+      website,
+    } = req.body;
+    await prisma.provider_details.create({
+      data: {
+        provider_name: providerName,
+        provider_type: providerType,
+        provider_handle: providerHandle,
+        about,
+        address,
+        city,
+        state,
+        country,
+        postal_code: postalCode,
+        owner,
+        website,
+      },
+    });
 
-        // Generate the QR code as a data URL
-        const qrCodeDataURL = await qrcode.toDataURL(process.env.BASE_URL + providerHandle,
-            {
-                color: {
-                    dark: '#f15800', // Primary Colour
-                    light: '#0000' // Transparent background
-                }
-            });
+    // Generate the QR code as a data URL
+    const qrCodeDataURL = await qrcode.toDataURL(
+      process.env.BASE_URL + providerHandle,
+      {
+        color: {
+          dark: "#f15800", // Primary Colour
+          light: "#0000", // Transparent background
+        },
+      }
+    );
 
-        resUtil(
-            res,
-            200,
-            'Provider was successfully registered.',
-            { qrCodeDataURL: qrCodeDataURL }
-        )
-    } catch (error) {
-        console.error('Error creating provider:', error);
-        resUtil(
-            res,
-            500,
-            'An error occurred.'
-        )
-    }
-}
+    resUtil(res, 200, "Provider was successfully registered.", {
+      qrCodeDataURL: qrCodeDataURL,
+    });
+  } catch (error) {
+    console.error("Error creating provider:", error);
+    resUtil(res, 500, "An error occurred.");
+  }
+};
 
-module.exports = allowCors(handler)
+module.exports = allowCors(handler);
