00f1a379393c7dcfd36e651cb2795421acf73c50
xav1994das <xav0anime@gmail.com>
2023-07-23 13:07:36 +0530

    test

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 import Router from "router";
 import finalhandler from "finalhandler";
-import { createMenuItem } from "../services/createMenuItem";
+const getMenu = require("../services/getMenu");
+const createMenuItem = require("../services/createMenuItem");
 import { createProvider } from "../services/createProvider";
 import { deleteMenuItems } from "../services/deleteMenuItems";
 import { deleteProvider } from "../services/deleteProvider";
 import { getFeedback } from "../services/getFeedback";
-import { getMenu } from "../services/getMenu";
 import { getMenuItems } from "../services/getMenuItems";
 import { getProviderDetails } from "../services/getProviderDetails";
 import { getProviders } from "../services/getProviders";
@@ -14,10 +14,10 @@ import { updateProvider } from "../services/updateProvider";
 
 const router = Router();
 
-router.get("/api/root/get-menu", getMenu);
+router.get("/api/get-menu", getMenu);
+router.get("/api/create-menu-item", createMenuItem);
 
 function getRoutes(req, res) {
-  console.log("hi");
   router(req, res, finalhandler(req, res));
 }
 
