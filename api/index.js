bc1fbcdfd385632765ad563fd68f63a337980a9e
Maharshi Bhaduri <bhaduri.maharshi@gmail.com>
2025-04-27 17:07:13 +0530

    added update and get billing details api

--- api/index.js
+++ api/index.js
@@ -24,6 +24,7 @@ const updateOrderPartner = require("../services/updateOrderPartner");
 const updateFeedbackPartner = require("../services/updateFeedbackPartner");
 const updateConfig = require("../services/updateConfig");
 const requestBill = require("../services/requestBill");
+const getBillConfig = require("../services/getBillConfig");
 const router = Router();
 
 router.post("/api/create-provider", createProvider);
@@ -50,6 +51,7 @@ router.post("/api/update-order-partner", updateOrderPartner);
 router.post("/api/update-feedback-partner", updateFeedbackPartner);
 router.post("/api/update-config", updateConfig);
 router.post("/api/request-bill", requestBill);
+router.get("/api/get-bill-config", getBillConfig);
 
 function getRoutes(req, res) {
   router(req, res, finalhandler(req, res));
